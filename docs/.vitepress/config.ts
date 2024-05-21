86672198605897b02cdcca1ed67a2f1ca8fd3196
Broering <62901166+orchestralblend@users.noreply.github.com>
2024-05-20 22:38:56 -0300

    White favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,7 +13,7 @@ export default {
     [
       'link', 
       { 
-        rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+        rel: 'icon', type: 'image/x-icon', href: '/favicon-dark.ico'
       }
     ],
     [
