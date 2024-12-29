d90da3c5c025b6de5e2e155809ba5cebb7d32592
August Resende <me@augustresende.com>
2024-12-29 19:32:15 -0300

    Redireciona visitantes no domínio antigo para o novo

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,6 +14,16 @@ export default {
         rel: 'icon', media: '(prefers-color-scheme: dark)', href: '/favicon-dark.ico'
       }
     ],
+    [
+      'script',
+      {},
+      `
+        if (window.location.hostname === "megathread.ahoypirata.lat") {
+            const newURL = window.location.href.replace("megathread.ahoypirata.lat", "pirataria.link");
+            window.location.href = newURL;
+        }
+      `
+    ]
   ],
   vite: {
     plugins: [OramaPlugin()],
