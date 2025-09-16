c53c6caeb3e303e145178de71db4fc516d534458
Felipe Maciel <srstevestifler@gmail.com>
2025-09-16 14:03:53 -0300

    Corrige erro de digitação no banner do endof10

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -8,7 +8,7 @@ const { Layout } = DefaultTheme
   <Layout>
     <template #home-features-after>
 <section class="endof10"><a href="https://endof10.org/pt-br"><svg viewBox="0 0 688.235 754"><rect width="260.474" height="260.474" x="-36.543" y="129.876" opacity=".4" rx="43.06" style="stroke-width:.920077" transform="rotate(-15.111)"></rect><rect width="260.474" height="260.474" x="407.612" y="96.465" opacity=".4" rx="43.06" style="stroke-width:.920077"></rect><rect width="260.474" height="260.474" x="117.716" y="386.36" opacity=".4" rx="43.06" style="stroke-width:.920077"></rect><rect width="260.474" height="260.474" x="486.185" y="332.836" opacity=".4" rx="43.06" style="stroke-width:.920077" transform="rotate(7.911)"></rect></svg>
-<span>Continue usando seu PC após o término do suporte do Windows 11 agora em outubro. <strong>Visite endof10.org/pt-br</strong></span></a></section>
+<span>Continue usando seu PC após o término do suporte do Windows 10 agora em utubro. <strong>Visite endof10.org/pt-br</strong></span></a></section>
     </template>
   </Layout>
 </template>
